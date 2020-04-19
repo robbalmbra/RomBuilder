6ec41a633a19f7de6b70f891352277ef6d0fce6c
Robert Balmbra <robbalmbra@gmail.com>
2020-04-19 12:32:46 +0100

    Update start-local.sh

--- scripts/start-local.sh
+++ scripts/start-local.sh
@@ -15,42 +15,35 @@ error_exit()
 CURRENT="$(pwd)"
 
 # Requirements for buildkite
-if [[ ! -z "${BUILDKITE}" ]]; then
-  if [ ! -d "/opt/build_env" ]; then
-    echo "Error - Build tools dont exist on host, run setup-buildkite.sh to install required packages"
-    exit 1
+
+if [ -f "/etc/lsb-release" ] && [ ! -d "/opt/build_env" ]; then
+  # Linux install
+  echo "Pulling and installing tools"
+  git clone https://github.com/akhilnarang/scripts.git /opt/build_env --depth=1
+  sudo chmod +x /opt/build_env/setup/android_build_env.sh
+  . /opt/build_env/setup/android_build_env.sh
+
+  apt-get -y upgrade > /dev/null 2>&1 && \
+  apt-get -y install make python3 bc bison git screen wget openjdk-8-jdk python-lunch lsb-core sudo curl shellcheck \
+  autoconf libtool g++ libcrypto++-dev build-essential libz-dev libsqlite3-dev libssl-dev libcurl4-gnutls-dev libreadline-dev \
+  libpcre++-dev libsodium-dev libc-ares-dev libfreeimage-dev libavcodec-dev libavutil-dev libavformat-dev flex \
+  libswscale-dev libmediainfo-dev libzen-dev libuv1-dev libxkbcommon-dev libxkbcommon-x11-0 zram-config \
+  libelf-dev libncurses-dev g++-multilib gcc-multilib gperf libxml2 libxml2-utils zlib1g-dev zip yasm \
+  squashfs-tools xsltproc schedtool rsync lzop liblz4-tool libesd0-dev lib32z1-dev lib32readline-dev libsdl1.2-dev > /dev/null 2>&1   
+
+  # Download and build mega
+  if [ ! -d "/opt/MEGAcmd/" ]; then
+    wget --quiet -O /opt/megasync.deb https://mega.nz/linux/MEGAsync/xUbuntu_$(lsb_release -rs)/amd64/megasync-xUbuntu_$(lsb_release -rs)_amd64.deb && ls /opt/ && dpkg -i /opt/megasync.deb
+    cd /opt/ && git clone --quiet https://github.com/meganz/MEGAcmd.git
+    cd /opt/MEGAcmd && git submodule update --quiet --init --recursive && sh autogen.sh > /dev/null 2>&1 && ./configure --quiet && make > /dev/null 2>&1 && make install > /dev/null 2>&1
   fi
+
+  apt update --fix-missing
+  sudo apt install -f
+
 else
-  # Install build tools
-  if [ -f "/etc/lsb-release" ] && [ ! -d "/opt/build_env" ]; then
-    # Linux install
-    echo "Pulling and installing tools"
-    git clone https://github.com/akhilnarang/scripts.git /opt/build_env --depth=1
-    sudo chmod +x /opt/build_env/setup/android_build_env.sh
-    . /opt/build_env/setup/android_build_env.sh
-
-    apt-get -y upgrade > /dev/null 2>&1 && \
-    apt-get -y install make python3 bc bison git screen wget openjdk-8-jdk python-lunch lsb-core sudo curl shellcheck \
-    autoconf libtool g++ libcrypto++-dev build-essential libz-dev libsqlite3-dev libssl-dev libcurl4-gnutls-dev libreadline-dev \
-    libpcre++-dev libsodium-dev libc-ares-dev libfreeimage-dev libavcodec-dev libavutil-dev libavformat-dev flex \
-    libswscale-dev libmediainfo-dev libzen-dev libuv1-dev libxkbcommon-dev libxkbcommon-x11-0 zram-config \
-    libelf-dev libncurses-dev g++-multilib gcc-multilib gperf libxml2 libxml2-utils zlib1g-dev zip yasm \
-    squashfs-tools xsltproc schedtool rsync lzop liblz4-tool libesd0-dev lib32z1-dev lib32readline-dev libsdl1.2-dev > /dev/null 2>&1   
-
-    # Download and build mega
-    if [ ! -d "/opt/MEGAcmd/" ]; then
-      wget --quiet -O /opt/megasync.deb https://mega.nz/linux/MEGAsync/xUbuntu_$(lsb_release -rs)/amd64/megasync-xUbuntu_$(lsb_release -rs)_amd64.deb && ls /opt/ && dpkg -i /opt/megasync.deb
-      cd /opt/ && git clone --quiet https://github.com/meganz/MEGAcmd.git
-      cd /opt/MEGAcmd && git submodule update --quiet --init --recursive && sh autogen.sh > /dev/null 2>&1 && ./configure --quiet && make > /dev/null 2>&1 && make install > /dev/null 2>&1
-    fi
-
-    apt update --fix-missing
-    sudo apt install -f
-
-  else
-    # MacOS install - todo
-    echo ""
-  fi
+  # MacOS install - todo
+  echo ""
 fi
 
 # Sets vars for run script
