9839c1b8b73ee2b249792be5eb2dcedd9bb03614
Robert Balmbra <robbalmbra@gmail.com>
2020-04-19 18:21:04 +0100

    Update buildkite_logger.sh

--- scripts/buildkite_logger.sh
+++ scripts/buildkite_logger.sh
@@ -4,19 +4,21 @@
 
 LOG_FILE=$1
 SLEEP_WAIT=$2
+last_line=""
 
 while [ 1 ]
 do
-  last_line=$(tail -n 1 $LOG_FILE)
-  
+  line=$(tail -n 1 $LOG_FILE)
+
   # Exit if build is marked as complete
-  if [ $last_line -eq "BUILD_COMPLETE" ]; then
+  if [ "$line" -eq "BUILD_COMPLETE" ] && [ "$line" -ne "$last_line" ]; then
     break
   fi
-  
+
   # Print line
-  echo $last_line
-  
+  last_line="$line"
+  echo $line
+
   # Wait
   sleep $SLEEP_WAIT
 done
