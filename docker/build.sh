acb433f37eaf2e9172b4fa9afa022631fa526bd4
Robert Balmbra <robbalmbra@gmail.com>
2020-04-20 11:45:50 +0100

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -229,7 +229,7 @@ fi
 # Execute specific user modifications and environment specific options if avaiable
 if [ -f "$BUILD_DIR/user_modifications.sh" ]; then
   echo "Using user modification script"
-  $BUILD_DIR/user_modifications.sh $BUILD_DIR 1> /dev/null
+  $BUILD_DIR/user_modifications.sh "$BUILD_DIR" 1> /dev/null
   error_exit "user modifications"
 fi
 
