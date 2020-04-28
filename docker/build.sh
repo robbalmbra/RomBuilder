384da1e2103c49f36886c522b2bf8a4ac51c0b6d
robbbalmbra <robbalmbra@gmail.com>
2020-04-28 15:23:06 +0100

    added support for enabling and disabling libexynoscamera

--- docker/build.sh
+++ docker/build.sh
@@ -3,7 +3,7 @@
 # Check for props environment variable to add to build props
 if [ ! -z $ADDITIONAL_PROPS ]; then
   export IFS=";"
-  additional_props_string="PRODUCT_PRODUCT_PROPERTIES += \\\"
+  additional_props_string="PRODUCT_PRODUCT_PROPERTIES += \\\\"
   additional_props_string+="\n"
   for prop in $ADDITIONAL_PROPS; do
     echo "Adding additional prop '$prop'"
