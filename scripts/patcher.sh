298a18891cf7c071df66f1a76799ca99da0743b8
Robert Balmbra <robbalmbra@gmail.com>
2020-04-29 19:54:38 +0100

    Update patcher.sh

--- scripts/patcher.sh
+++ scripts/patcher.sh
@@ -40,6 +40,7 @@ unzip $rom_file_in -d $WORK_DIR > /dev/null 2>&1
 # Add magisk to updater script
 update_script="$WORK_DIR/META-INF/com/google/android/updater-script"
 
+if [ $MAGISK_IN_BUILD -eq 1 ]; then
 cat <<EOT >> $update_script
 ui_print("-- Installing: Magisk");
 package_extract_dir("META-INF/ADD-ONS/Magisk", "/tmp/Magisk");
@@ -47,6 +48,16 @@ run_program("/sbin/busybox", "unzip", "/tmp/Magisk/Magisk.zip", "META-INF/com/go
 run_program("/sbin/busybox", "sh", "/tmp/Magisk/META-INF/com/google/android/update-binary", "dummy", "1", "/tmp/Magisk/Magisk.zip");
 delete_recursive("/tmp/Magisk");
 EOT
+
+# Make magisk addon directory
+magisk_dir=$WORK_DIR/META-INF/ADD-ONS/Magisk
+mkdir -p "$magisk_dir"
+
+# Get latest magisk version
+magisk_url="https://github.com/topjohnwu/Magisk/releases/download/v$magisk_version/Magisk-v$magisk_version.zip"
+echo "Downloading $magisk_url"
+wget $magisk_url -O $magisk_dir/Magisk.zip > /dev/null 2>&1
+fi
 
 if [ $LIBEXYNOS_CAMERA -eq 1 ]; then
 cat <<EOT2 >> $update_script
@@ -57,23 +68,14 @@ run_program("/sbin/busybox", "mv", "/tmp/libexynoscamera3/libexynoscamera3.so", 
 run_program("/sbin/busybox", "umount", "/vendor");
 delete_recursive("/tmp/libexynoscamera3");
 EOT2
-fi
 
-# Create directory structure
-magisk_dir=$WORK_DIR/META-INF/ADD-ONS/Magisk
+# Make libexynos addon directory
 libexynoscamera_dir=$WORK_DIR/META-INF/ADD-ONS/libexynoscamera3
-mkdir -p "$magisk_dir"
 mkdir -p "$libexynoscamera_dir"
 
-# Get libexynoscamera for device
-if [ $LIBEXYNOS_CAMERA -eq 1 ]; then
-  cp $building_dir/supplements/libexynoscamera3/libexynoscamera3-$device_name.so $libexynoscamera_dir/libexynoscamera3.so
+# Copy device lib to addon directory
+cp $building_dir/supplements/libexynoscamera3/libexynoscamera3-$device_name.so $libexynoscamera_dir/libexynoscamera3.so
 fi
-
-# Get latest magisk version
-magisk_url="https://github.com/topjohnwu/Magisk/releases/download/v$magisk_version/Magisk-v$magisk_version.zip"
-echo "Downloading $magisk_url"
-wget $magisk_url -O $magisk_dir/Magisk.zip > /dev/null 2>&1
 
 # Zip folder to output directory
 rm -rf "$rom_folder_out/$file_name"
