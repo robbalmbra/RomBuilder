7a09e2be7883c406ce69ed27d3f3e4e8e335fb3f
Robert Balmbra <robbalmbra@gmail.com>
2020-05-02 18:05:00 +0100

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -244,7 +244,7 @@ else
   echo "Syncing sources from repo"
 
   if [[ ! -z "${BUILDKITE}" ]]; then
-    repo sync -d -f -c -j$MAX_CPU --force-sync --no-clone-bundle --no-tags
+    repo sync -d -f -c -j$MAX_CPU --force-sync --no-clone-bundle --no-tags --quiet > /dev/null 2>&1
     error_exit "repo sync"
   else
     repo sync -d -f -c -j$MAX_CPU --force-sync --no-clone-bundle --no-tags --quiet
