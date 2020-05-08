17d9c7e6f6ab11c7b522b4a2bc5347bae7177fc6
Robert Balmbra <robbalmbra@gmail.com>
2020-05-08 17:16:58 +0100

    Update main.sh

--- supplements/ota/main.sh
+++ supplements/ota/main.sh
@@ -45,13 +45,13 @@ for ROM in $rom_folder/out/target/product/*/*.zip; do
   DEVICE="$(basename "$(dirname "$ROM")")"
 
   # Run specific build ota generation script
-  python3 "$rom_name.py" "$ROM" "$rom_name" 1> "$ota_folder/$DEVICE.json" 2> /dev/null
+  date=$(date '+%d-%m-%y')
+  python3 "$rom_name.py" "$ROM" "$rom_name" "$date" 1> "$ota_folder/$DEVICE.json" 2> /dev/null
 
   # Make device directory
   sftp_mkdir "/home/frs/project/evo9810ota/$rom_name/$DEVICE/"
 
   # Create date directory in each device
-  date=$(date '+%d-%m-%y')
   sftp_mkdir "/home/frs/project/evo9810ota/$rom_name/$DEVICE/$date/"
 
   # Upload rom file to sourceforge
