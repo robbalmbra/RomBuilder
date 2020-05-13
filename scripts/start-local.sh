c68a79f5fb4270430d7a8bb0ff17c7d8043a943d
Robert Balmbra <robbalmbra@gmail.com>
2020-05-13 14:31:05 +0100

    Update start-local.sh

--- scripts/start-local.sh
+++ scripts/start-local.sh
@@ -47,12 +47,12 @@ if [ -f "/etc/lsb-release" ] && [ ! -d "/opt/build_env" ]; then
   
   apt-get install -y git curl > /dev/null 2>&1
   
-  git clone https://github.com/akhilnarang/scripts.git /opt/build_env --depth=1
+  git clone https://github.com/akhilnarang/scripts.git /opt/build_env --depth=1 
   sudo chmod +x /opt/build_env/setup/android_build_env.sh
   . /opt/build_env/setup/android_build_env.sh
 
   apt-get -y upgrade > /dev/null 2>&1 && \
-  apt-get -y install make python3 bc bison git screen wget openjdk-8-jdk python-lunch lsb-core sudo curl shellcheck \
+  apt-get -y install make python3 bc bison git screen wget openjdk-8-jdk lsb-core sudo curl shellcheck \
   autoconf libtool g++ libcrypto++-dev build-essential libz-dev libsqlite3-dev libssl-dev libcurl4-gnutls-dev libreadline-dev \
   libpcre++-dev libsodium-dev libc-ares-dev libfreeimage-dev libavcodec-dev libavutil-dev libavformat-dev flex \
   libswscale-dev libmediainfo-dev libzen-dev libuv1-dev libxkbcommon-dev libxkbcommon-x11-0 zram-config python3-pip \
