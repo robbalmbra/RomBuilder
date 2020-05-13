9bf997cadae3ad590bbf6aae83454a51d3ee4abe
Robert Balmbra <robbalmbra@gmail.com>
2020-05-13 14:24:55 +0100

    Update setup-buildtools.sh

--- scripts/setup-buildtools.sh
+++ scripts/setup-buildtools.sh
@@ -35,7 +35,7 @@ error_exit "environment script"
 
 echo "Installing apt packages"
 apt-get -y upgrade > /dev/null 2>&1 && \
-apt-get -y install make python3 git screen wget openjdk-8-jdk python-lunch lsb-core sudo curl shellcheck \
+apt-get -y install make python3 git screen wget openjdk-8-jdk lsb-core sudo curl shellcheck \
 autoconf libtool g++ libcrypto++-dev libz-dev libsqlite3-dev libssl-dev libcurl4-gnutls-dev libreadline-dev \
 libpcre++-dev libsodium-dev libc-ares-dev libfreeimage-dev libavcodec-dev libavutil-dev libavformat-dev python3-pip \
 libswscale-dev libmediainfo-dev libzen-dev libuv1-dev libxkbcommon-dev libxkbcommon-x11-0 zram-config
