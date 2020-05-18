9791d99383ce7407e77e8ffa4fac8dbe5fe8b3bb
Robert Balmbra <robbalmbra@gmail.com>
2020-05-18 13:19:49 +0100

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -561,7 +561,7 @@ if [ "$SKIP_BUILD" -eq 0 ]; then
     fi
 
     # Run lunch
-    build_id="${BUILD_NAME}_$DEVICE-$LUNCH_DEBUG"
+    build_id="${BUILD_NAME}_$DEVICE-${LUNCH_DEBUG}"
     if [[ ! -z "${CUSTOM_LUNCH_COMMAND}" ]]; then
       eval "${CUSTOM_LUNCH_COMMAND}" "$build_id" > /dev/null 2>&1
     else
