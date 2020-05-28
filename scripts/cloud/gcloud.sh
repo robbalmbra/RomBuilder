e4b02b4370e23b7afe76f1af71d57362296fa65d
Robert Balmbra <robbalmbra@gmail.com>
2020-05-29 00:07:02 +0100

    Update gcloud.sh

--- scripts/cloud/gcloud.sh
+++ scripts/cloud/gcloud.sh
@@ -159,8 +159,8 @@ if [ $? -eq 0 ]; then
   done
 
   # Copy private config to instance if its exists on host
-  if [ -f "$HOME/.rom.env" ]; then
-    scp -o StrictHostKeyChecking=no "$HOME/.rom.env" ubuntu@$public_ip:/tmp/rom.env > /dev/null 2>&1
+  if [ -f "$HOME/rom.env" ]; then
+    scp -o StrictHostKeyChecking=no "$HOME/rom.env" ubuntu@$public_ip:/tmp/rom.env > /dev/null 2>&1
   fi
 
   echo "Complete"
