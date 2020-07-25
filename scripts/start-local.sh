d019d166cb13adf7fec1035f8b05aa983b884f7b
Robert Balmbra <robbalmbra@gmail.com>
2020-07-25 09:45:11 +0100

    Update start-local.sh

--- scripts/start-local.sh
+++ scripts/start-local.sh
@@ -191,7 +191,7 @@ if [ "$scp_check" -eq 5 ]; then
     fi
 
     # Check connection to server via public key auth
-    ssh -o BatchMode=yes -o ConnectTimeout=5 ${SCP_USERNAME}@${SCP_HOST} > /dev/null 2>&1
+    ssh -i "$HOME/.ssh/id_rsa" -o BatchMode=yes -o ConnectTimeout=5 ${SCP_USERNAME}@${SCP_HOST} > /dev/null 2>&1
     ret=$?
     if [ $ret -gt 1 ]; then
       error_message "Failed to connect to '${SCP_HOST}' using username ${SCP_USERNAME}. Return code: $ret"
