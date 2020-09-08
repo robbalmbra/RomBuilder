e3451742b02a17b5e0a4c8f30b08a903467eb733
Robert Balmbra <robbalmbra@gmail.com>
2020-09-08 16:59:49 +0100

    Avoid duplicate entries for changelog with sort -u

--- scripts/create_changelog.sh
+++ scripts/create_changelog.sh
@@ -7,4 +7,4 @@ fi
 
 day_count=$1
 output_file=$2
-repo forall -pc git log --reverse --no-merges --since=$day_count.days.ago | grep "Date:" -A 2 | grep -v "Date:" | sed '/^--/d' | sed '/^$/d' | sed 's/^ *//g' > $output_file
+repo forall -pc git log --reverse --no-merges --since=$day_count.days.ago | grep "Date:" -A 2 | grep -v "Date:" | sed '/^--/d' | sed '/^$/d' | sed 's/^ *//g' | sort -u > $output_file
